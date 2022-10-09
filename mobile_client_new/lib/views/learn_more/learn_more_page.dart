26c1794b38f7e40499edca48b4045abc98139ea8
Ensar Emir EROL <ensaremir.erol99@gmail.com>
2022-10-09 22:51:46 +0300

    Learn more page refactored

--- mobile_client_new/lib/views/learn_more/learn_more_page.dart
+++ mobile_client_new/lib/views/learn_more/learn_more_page.dart
@@ -1,10 +1,12 @@
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:mobile_client_new/views/learn_more/widgets/content_item.dart';
 import 'package:mobile_client_new/views/learn_more/widgets/table_of_content_item.dart';
 import 'package:mobile_client_new/widgets/responsive_layout/responsive_layout.dart';
+
+part 'views/learn_more_small.dart';
+part 'views/learn_more_large.dart';
 
 class LearnMorePage extends StatefulWidget {
   const LearnMorePage({super.key});
@@ -68,74 +70,6 @@ class _LearnMorePageState extends State<LearnMorePage> {
       content.addAll(subContents.content);
     }
     return _Contents(tableOfContents, content);
-  }
-}
-
-class _LearnMoreLarge extends StatelessWidget {
-  const _LearnMoreLarge({super.key, required this.contents});
-  final _Contents contents;
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text('Learn More'),
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              if (MediaQuery.of(context).size.width > 300)
-                Expanded(
-                  flex: 1,
-                  child: SingleChildScrollView(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: contents.tableOfContents,
-                    ),
-                  ),
-                ),
-              Expanded(
-                flex: 3,
-                child: SingleChildScrollView(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: contents.content,
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ));
-  }
-}
-
-class _LearnMoreSmall extends StatelessWidget {
-  const _LearnMoreSmall({super.key, required this.contents});
-  final _Contents contents;
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Learn More'),
-      ),
-      endDrawer: Drawer(
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: contents.tableOfContents,
-          ),
-        ),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: contents.content,
-        ),
-      ),
-    );
   }
 }
 
---
+++ mobile_client_new/lib/views/learn_more/views/learn_more_small.dart
@@ -0,0 +1,27 @@
+part of '../learn_more_page.dart';
+class _LearnMoreSmall extends StatelessWidget {
+  const _LearnMoreSmall({super.key, required this.contents});
+  final _Contents contents;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Learn More'),
+      ),
+      endDrawer: Drawer(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: contents.tableOfContents,
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: contents.content,
+        ),
+      ),
+    );
+  }
+}
