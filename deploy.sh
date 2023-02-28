86336c96cd673b29555b62f9f9da1c787fee3e51
Vincent Emonet <vincent.emonet@gmail.com>
2023-02-28 14:35:03 +0100

    force flutter 2.19 and up

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Build with cache:
-ssh ids2 'cd /data/deploy-services/responsability-matrix-survey ; git pull ; docker-compose -f docker-compose.yml -f docker-compose.prod.yml up --force-recreate --build -d'
+# ssh ids2 'cd /data/deploy-services/responsability-matrix-survey ; git pull ; docker-compose -f docker-compose.yml -f docker-compose.prod.yml up --force-recreate --build -d'
 
 # Build without cache:
-# ssh ids2 'cd /data/deploy-services/knowledge-collaboratory ; git pull ; docker-compose -f docker-compose.yml -f docker-compose.prod.yml build --no-cache ; docker-compose -f docker-compose.yml -f docker-compose.prod.yml down ; docker-compose -f docker-compose.yml -f docker-compose.prod.yml up --force-recreate -d'
+ssh ids2 'cd /data/deploy-services/responsability-matrix-survey ; git pull ; docker-compose -f docker-compose.yml -f docker-compose.prod.yml build --no-cache ; docker-compose -f docker-compose.yml -f docker-compose.prod.yml down ; docker-compose -f docker-compose.yml -f docker-compose.prod.yml up --force-recreate -d'
