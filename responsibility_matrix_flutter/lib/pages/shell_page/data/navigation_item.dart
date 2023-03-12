529983bb30e8a1c45bdb9d1896d5e1bd03a9463e
Ensar Emir EROL <ensaremir.erol99@gmail.com>
2023-03-12 22:42:59 +0300

    flutter rewrite

---
+++ responsibility_matrix_flutter/lib/pages/shell_page/data/navigation_item.dart
@@ -0,0 +1,13 @@
+part of '../shell_page.dart';
+
+class NavigationItem {
+  final String title;
+  final int index;
+  final String route;
+
+  const NavigationItem({
+    required this.title,
+    required this.index,
+    required this.route,
+  });
+}
